597ada0fd457b4ebb114ade7b39d72d440583ecb
joseGCalle <joseeliecergrisales@gmail.com>
2021-09-07 22:09:53 -0500

    final changes

--- NewsAndWeatherAPI/Controllers/WeatherAndNewsController.cs
+++ NewsAndWeatherAPI/Controllers/WeatherAndNewsController.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace NewsAndWeatherAPI.Controllers
 {
--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -5,39 +5,33 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.baseUrl = "https://localhost:44363/WeatherAndNews?cityname=";
-    this.city = {value: ''};
-    this.state={
-      news: '',
-      weatherInfo: ''
-    };
+    this.state = {value: ''};
+    this.weather = {value:''};
     this.handleChange = this.handleChange.bind(this);
-
-    this.getNewsAndWeather= async function(city) {
-      console.log(city);
-      try {
-        await fetch(this.baseUrl+this.city.value)
-          .then(function (response) {
-            console.log(response.news);
-            return response.json();
-          })
-          .then(function (myjson) {
-            console.log(myjson.news.status);
-            this.state.news(myjson.news.status)
-          });
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
   }
+    
+  handleSubmit(event) {
+    event.preventDefault();
+    try {
+      fetch(this.baseUrl+this.state.value)
+      .then(function (response) {
+        console.log(response.WeatherInfo);
+        return response.json();
+      })
+      .then(function (myjson) {
+        console.log()
+        this.setWeather({value: myjson.WeatherInfo.name})
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-  // useEffect(() => {
-
-  //   getNewsAndWeather();
-  // }, [])
   
 render(){
   return (
@@ -45,13 +39,13 @@ render(){
       {
         <div className="container">
           <div className="row">
-            <form className="col s12">
+            <form className="col s12" onSubmit={this.handleSubmit}>
               <div className="row">
                 <div className="input-field col s6">
-                  <input id="search" type="text" className="validate" name="city" defaultValue={this.city.value} onChange={this.handleChange}></input>
+                  <input id="search" type="text" defaultValue={this.state.value} onChange={this.handleChange}></input>
                   <label htmlFor="search">Ingrese una ciudad</label>
                 </div>
-                <button className="waves-effect waves-light btn" onClick={() => this.getNewsAndWeather()}>Buscar</button>
+                <input type="submit" value="Submit" />
               </div>
             </form>
             <div className="row" id="responsecontent">
@@ -59,7 +53,7 @@ render(){
               <div className="col s12 m6">
                 <div className="card blue-grey darken-1">
                   <div className="card-content white-text">
-                    <span className="card-title">{this.state.news}</span>
+                    <span className="card-title">{this.weather.value}</span>
                     <p>I am a very simple card. I am good at containing small bits of information.
                     I am convenient because I require little markup to use effectively.</p>
                   </div>
